7d52e86f6af5d3b1bc272b637d414a9739674256
Theo <73323028+kkwondev@users.noreply.github.com>
2024-05-16 07:35:52 +0000

    #2.1

---
+++ app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import Avatar from "./components/avatar";
+
+export default function AboutUs() {
+  return (
+    <h1>
+      Hello About-us <Avatar />
+    </h1>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
-export default function Tomato() {
-  return <h1>hello nextjs</h1>;
+export default function Page() {
+  return <h1>Hello!</h1>;
 }
